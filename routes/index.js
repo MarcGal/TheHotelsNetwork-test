5d20c6eb4f87e2057266740f32a430cfd142c7e0
marcogallo <mgalloperalta@gmail.com>
2019-06-18 00:11:48 +0200

    Get current and tomorrow dates. pass it to view.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const today = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+  const currentDate = new Date(+new Date() + 86400000);
+  var day = currentDate.getDate()
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  const tomorrow = `${year}/${month}/${day}`
+  res.render('index', { today, tomorrow });
 });
 
 module.exports = router;
