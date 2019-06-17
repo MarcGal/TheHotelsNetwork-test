235a9bf23dc0cefd51959c5f053235f32cd010d2
marcogallo <mgalloperalta@gmail.com>
2019-06-18 00:27:00 +0200

    Refactor getDate functions

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const today = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  const currentDate = new Date(+new Date() + 86400000);
-  var day = currentDate.getDate()
-  var month = currentDate.getMonth() + 1
-  var year = currentDate.getFullYear()
-  const tomorrow = `${year}/${month}/${day}`
+  const today = new Date().toJSON().slice(0,10);
+  const tomorrow = new Date(+new Date() + 86400000).toJSON().slice(0,10);
+  // var day = tomorrow.getDate()
+  // var month = currentDate.getMonth() + 1
+  // var year = currentDate.getFullYear()
+  // const tomorrow = `${year}-${month}-${day}`
   res.render('index', { today, tomorrow });
 });
 
