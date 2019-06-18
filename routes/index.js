761ecbb11b04c633915d0a081bfdd9559354218e
marcogallo <mgalloperalta@gmail.com>
2019-06-18 21:56:50 +0200

    Send sorted accomodations to fronend

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const accommodations = require('../public/assets/accomdations');
+const sortAccommodations = require('../helpers/sortAccommodation');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   const today = new Date().toJSON().slice(0,10);
   const tomorrow = new Date(+new Date() + 86400000).toJSON().slice(0,10);
-  res.render('index', { today, tomorrow });
+  const accommodationSelection = await accommodations.filter( accommodation => { return accommodation.people <= 2});
+  await sortAccommodations(accommodationSelection);
+  res.render('index', { today, tomorrow, accommodationSelection });
 });
 
 module.exports = router;
