4e641c818534011b3aecf4bda046896894f2b305
marcogallo <mgalloperalta@gmail.com>
2019-06-18 20:46:13 +0200

    Build accomodation static file with house options

---
+++ public/assets/accomdations.js
@@ -0,0 +1,94 @@
+const accomodations = [
+
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 140,
+      beds: 7,
+      people: 10,
+      price: 700,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 75,
+      beds: 4,
+      people: 4,
+      price: 200,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 80,
+      beds: 2,
+      people: 4,
+      price: 300,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 140,
+      beds: 7,
+      people: 10,
+      price: 250,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 40,
+      beds: 1,
+      people: 2,
+      price: 150,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 100,
+      beds: 3,
+      people: 5,
+      price: 1000,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Spatious atic in the center",
+      description: "",
+      size: 80,
+      beds: 3,
+      people: 3,
+      price: 380,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Luxury loft for two",
+      description: "",
+      size: 60,
+      beds: 2,
+      people: 2,
+      price: 250,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+    
+  },
+  { 
+      title: "Small but cozy",
+      description: "",
+      size: 25,
+      beds: 2,
+      people: 2,
+      price: 180,
+      photo:'https://ar.rdcpix.com/2100210516/f2f10ad55dd38f1936510e2dcd6a2012c-f0xd-w480_h480_q80.jpg',
+  
+  },
+];
+
+module.exports = accomodations;
