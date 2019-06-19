895ea36aa25304fdd304a2a1b45490f90b53b17e
marcogallo <mgalloperalta@gmail.com>
2019-06-19 19:31:27 +0200

    Create reserve route

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const sortAccommodations = require('../helpers/sortAccommodation');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+  //DEFAULT DATA ON FIRST LOAD
   const checkin = new Date().toJSON().slice(0,10);
   const checkout = new Date(+new Date() + 86400000).toJSON().slice(0,10);
+  const adults = 1;
+  const children = 1;
   const accommodationSelection = await accommodations.filter( accommodation => { return accommodation.people <= 2});
-  await sortAccommodations(accommodationSelection);
-  res.render('index', { checkin, checkout, accommodationSelection });
+  await sortAccommodations(accommodationSelection); 
+  res.render('index', { checkin, checkout, accommodationSelection, adults, children });
 });
 
 router.get('/modify', async (req, res, next) => {
@@ -20,6 +23,15 @@ router.get('/modify', async (req, res, next) => {
   const accommodationSelection = await accommodations.filter( accommodation => { return accommodation.people >= people});
   await sortAccommodations(accommodationSelection);
   res.render('index', { checkin, checkout, adults, children, accommodationSelection });
+});
+
+router.get('/reserve/:price/:title/:checkin/:checkout/:adults/:children/:promoCode', async (req, res, next) => {
+  const { promoCode } = req.query;
+  const {price, title, checkin, checkout, adults, children } = req.params;
+  console.log(price, checkin, checkout, adults, children);
+  console.log(promoCode)
+  
+  res.render('checkout', { price, title, checkin, checkout, adults, children });
 });
 
 module.exports = router;
